79cda7ed2fa55365cb9a70e7dd37fedb0e7b04c9
sakshi <s.upadhyay1607@gmail.com>
2025-08-07 17:15:27 +0530

    initial commit

---
+++ HACKERRANK/WEEK_1/There is a collection of input strings and a collection of query strings. For each query string, determine how many times it occurs in the list of input strings. Return an array of the results.c
@@ -0,0 +1,58 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    result = []
+    frequency = {}
+
+    # Count the frequency of each string
+    for s in strings:
+        frequency[s] = frequency.get(s, 0) + 1
+
+    # Get the count for each query
+    for q in queries:
+        result.append(frequency.get(q, 0))
+
+    return result
+    
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
